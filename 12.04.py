f4afe5423d064a632125d490ab6ef3f3f6469614
YSW <fire4ice4@gmail.com>
2023-12-11 17:45:37 +0900

    LEC1211

--- 12.04.py
+++ 12.04.py
@@ -473,10 +473,10 @@ plt.colorbar() """
 
 # Histogram
 # plt.hist({data}, bins={구간수})
-import numpy as np
+# import numpy as np
 
-rn = np.random.randint(1,30,size=20)
-print(rn)
+# rn = np.random.randint(1,30,size=20)
+# print(rn)
 
 # plt.hist(rn, bins=10,label="def")
 
@@ -497,8 +497,8 @@ print(rn)
 # plt.legend()
 
 #Pie_Chart
-rate = [30, 40, 20, 10]
-labels = ["Alpha", "Beta", "Gamma", "Delta"]
+# rate = [30, 40, 20, 10]
+# labels = ["Alpha", "Beta", "Gamma", "Delta"]
 
 # plat.pie({data})
 # plt.pie(rate)
@@ -613,7 +613,7 @@ ax2.legend(loc="lower right") """
 
 
 # 이종 그래프 출력
-x = [1,4,5,8,9]
+""" x = [1,4,5,8,9]
 y1 = [2,3,6,7,10]
 y2 = [2,3,6,7,10]
 
@@ -628,9 +628,212 @@ ax2.bar(x, y2, color="C2",label="YData")
 ax2.set_ylabel("Y2data")
 
 ax1.legend()
-ax2.legend()
+ax2.legend() """
 
 
 
 # plt.tight_layout()
+# plt.show()
+
+
+#########################################
+#########################################
+### 23.12.11
+
+## 다중 그래프 출력
+# 사용데이터
+""" x1 = [2,3,6,7,10]
+x2 = [1,4,5,8,9]
+
+y1 = [1,4,5,8,9]
+y2 = [2,4,6,8,10]
+
+plt.subplot(2, 1, 1)    # 1set
+# plt.subplot(1, 2, 1)
+# plt.subplot(3, 1, 1)
+# plt.subplot(2, 2, 1)
+plt.plot(x1, y1, "o-")
+
+
+
+plt.title("X1 Graph")
+plt.xlabel("x-data")
+plt.ylabel("y-data")
+
+plt.subplot(2, 1, 2)    # 1set
+# plt.subplot(1, 2, 2)
+# plt.subplot(3, 1, 2)
+# plt.subplot(2, 2, 2)
+
+plt.plot(x2, y2, ".-")
+
+plt.title("X2 Graph")
+plt.xlabel("x2-data")
+plt.ylabel("y2-data")
+
+# Style 
+plt.style.use("bmh")
+
+# 결과 이미지 저장
+plt.savefig("data/img.jpg")
+plt.savefig("data/img.png")
+
+plt.show() """
+
+
+# 다중 객체를 이용한 막대 그래프 출력
+
+""" x_years = ["2020", "2021", "2022"]
+y_data = [100, 400, 900]
+
+# fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2)
+fig, ((ax1, ax3), (ax2, ax4)) = plt.subplots(2, 2)
+# ax1.bar(x_years, y_data)
+# ax2.bar(x_years, y_data)
+# ax3.bar(x_years, y_data)
+# ax4.bar(x_years, y_data)
+
+ax1.bar(x_years, y_data, color ="aquamarine", edgecolor = "black", hatch = "/")
+ax2.bar(x_years, y_data, color ="yellow", edgecolor = "black", hatch = "\\")
+ax3.bar(x_years, y_data, color ="salmon", edgecolor = "black", hatch = "*")
+ax4.bar(x_years, y_data, color ="lightskyblue", edgecolor = "black", hatch = "+")
+
+plt.tight_layout()
+
+plt.show() """
+
+########################################################
+
+### 데이터 처리 활용
+
+# Seaborn 사용
+
+import FinanceDataReader as fdr
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+
+
+""" tips = sns.load_dataset("tips")
+
+sns.regplot(x="total_bill", y="tip", data=tips)
+
+plt.show() """
+
+# #
+# titanic = sns.load_dataset("titanic")
+
+# sns.barplot(x="sex", y="survived", hue="pclass", data=titanic)
+
+# 탑승클래스별 인원구성
+# sns.countplot(x="class", hue="who", data=titanic)
+
+# 클래스별 생존자
+# sns.countplot(x="class", hue="alive", data=titanic)
+
+# 성별별 생존자
+# sns.countplot(x="sex", hue="alive", data=titanic)
+
+# 싱글 여행자별 인원구성
+# sns.countplot(x="alone", hue="who", data=titanic)
+
+# 탑승지별 생존자의 클래스 구별
+# sns.barplot(x="embark_town", y="survived", hue="pclass", data=titanic)
+
+# sns.countplot(x="smoker", hue="who", data=titanic)
+
+""" plt.title("Average Tips")
+plt.xlabel("x")
+plt.ylabel("Average")
+plt.show() """
+
+## 주식 데이터 분석
+
+import requests
+from datetime import datetime
+from matplotlib import dates as mdates
+from bs4 import BeautifulSoup as bs
+
+""" # 해당 데이터를 가져오는 URL
+# code 는 종목명
+url = "https://finance.naver.com/item/sise_day.nhn?code={}"
+
+df = fdr.DataReader("KS11")
+df0 = df.loc["2023"]
+
+df0["Close"].plot()
+
+plt.grid(True)
+
+# 다우지수와 코스피 비교
+dow = fdr.DataReader("DJI", "2010-06-01")
+kospi = fdr.DataReader("KS11", "2010-06-01")
+
+# 각 지수별 종가 기준 그래프 출력
+plt.plot(dow.index, dow.Close, "r--", label="Dow")
+plt.plot(kospi.index, kospi.Close, "b", label="KOSPI")
+
+## 국내 개별 종목 분석
+# 국내 주식 코드 확인
+# 해당 데이터를 가져오는 URL
+# code 는 종목명
+url = "https://finance.naver.com/item/sise_day.nhn?code={}"
+
+# data pathing
+headers = {"user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.96 Safari/537.36"}
+res = requests.get(url, headers=headers)
+html = bs(res.text, "html.parser")
+html_table = html.select("table")
+table = pd.read_html(str(html_table))
+
+
+df_list = []
+page = 1
+for page in range(1, 10):
+    page_url = f"{url}&page={page}"
+    print(page_url)
+
+    response = requests.get(page_url, headers=headers)
+    html = bs(response.text, "html.parser")
+    html_table = html.select("table")
+    table = pd.read_html(str(html_table))
+
+    if not table[0].empty:
+        df_list.append(table[0].dropna())
+        page += 1
+    else:
+        break
+
+print(df_list) """
+
+
+## 미국 개별 종목 분석
+
+import yfinance as yf
+
+#MS
+# ticker = "MSFT" #마소 주식 예시
+
+# Apple
+ticker = "APPL"  #애플 주식 예시
+start_date = "2022-01-01"
+end_date = "2023-12-02"
+
+data = yf.download(ticker, start=start_date, end=end_date)
+
+# 시각화
+plt.figure(figsize=(12,6))
+plt.plot(data["Close"], label="Close Price")
+
+# - 평균 계산
+# 50일 평균
+data["MA_50"] = data["Close"].rolling(window=50).mean()
+
+# 200일 평균
+data["MA_200"] = data["Close"].rolling(window=200).mean()
+
+plt.title("Stock Price")
+plt.xlabel("Data")
+plt.ylabel("Price($)")
+plt.legend()
 plt.show()
